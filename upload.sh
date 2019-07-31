c71051e8e0e9593aea6065299efd089c5a6322f4
hkunzhe <2571503172@qq.com>
2019-07-31 12:17:32 +0800

    add an upload shell script

---
+++ upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+add_files=$(git add -v . | awk '{print $2}' | awk -F "'" '{print $2}')
+message="upload"
+blank=" "
+for file in $add_files
+do
+    echo $file
+    message=$message$blank$file
+done
+
+if test $[$#] -eq $[0]
+then
+    echo $message
+    git commit -m $message
+else
+    echo $1
+    git commit -m "$1"
+fi
+
+git push origin master
+url="https://github.com/hkunzhe/picture/blob/master/"
+for file in $add_files
+do
+    echo $url$file
+done
